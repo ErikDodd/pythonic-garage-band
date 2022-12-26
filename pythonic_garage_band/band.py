77d97ab1ffc57e5fba08820da2c40999e7ef272d
ERIK M. DODD <emdodd27@gmail.com>
2022-12-26 10:45:25 -0800

    13/14 test passing. Probably need to refactor since code isn't very dry

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -4,8 +4,8 @@ class Band:
         self.name = name
         # Use a loop for the play_solos() to iterate over the list
 
-    # A Band instance should have a play_solos method that asks each member to play a solo, in the order they were added to band.
-    # play_solos():
+    # Band instance should have play_solos method that asks each member to play a solo, in the order added to band.
+
 
     def __str__(self):
         band_strings = [str(band) for band in self.musicians]
@@ -14,20 +14,31 @@ class Band:
     def __repr__(self):
         return f"Band({self.members}"
 
+
+    def play_solo(self, solo=""):
+        self.solo = solo
+        return f""
+
+
     # should have a class method to_list which returns a list of previously created Band instances
     def to_list(self):
         pass
 
 
 # base class
+
 class Musician:
     def __init__(self, name):
         self.name = name
 
-    # Hard-coded guitar. Need to figure out how to get all instruments there
-    def get_instrument(self, instrument):
+    def get_instrument(self, instrument=""):
         self.instrument = instrument
         return f"{instrument}"
+
+    # Each kind of Musician instance should have a play_solo method that returns string.
+    def play_solo(self, solo=""):
+        self.solo = solo
+        return f""
 
     def __str__(self):
         pass
@@ -35,16 +46,21 @@ class Musician:
     def __repr__(self):
         pass
 
-    # Each kind of Musician instance should have a play_solo method that returns string.
-
-    # play_solo():
-
 
 # derived class
 class Guitarist(Musician):
 
     def __init__(self, name):
+        self.solo = None
         self.name = name
+
+    def get_instrument(self, instrument="guitar"):
+        self.instrument = instrument
+        return f"{instrument}"
+
+    def play_solo(self, solo=""):
+        self.solo = solo
+        return f"face melting guitar solo"
 
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
@@ -57,6 +73,16 @@ class Bassist(Musician):
     def __str__(self):
         return f"My name is {self.name} and I play bass"
 
+
+    def get_instrument(self, instrument="bass"):
+        self.instrument = instrument
+        return f"{instrument}"
+
+
+    def play_solo(self, solo=""):
+        self.solo = solo
+        return f"bom bom buh bom"
+
     def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
 
@@ -65,13 +91,15 @@ class Drummer(Musician):
     def __str__(self):
         return f"My name is {self.name} and I play drums"
 
-    pass
+    def get_instrument(self, instrument="drums"):
+        self.instrument = instrument
+        return f"{instrument}"
+
+
+    def play_solo(self, solo=""):
+        self.solo = solo
+        return f"rattle boom crash"
+
 
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
-
-# Band (derived class)
-# class Nirvana(Band):
-# def __int__(self, name="Nirvana"):
-# self.name = name
-# pass
